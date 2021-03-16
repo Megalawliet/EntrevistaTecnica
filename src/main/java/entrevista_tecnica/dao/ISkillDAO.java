d4dc617c1685274ee63e34876890954899336329
Noelia Maria Barrera Bedós <galowmere@gmail.com>
2021-03-16 15:41:54 +0100

    Test Skill dao

--- src/main/java/entrevista_tecnica/dao/ISkillDAO.java
+++ src/main/java/entrevista_tecnica/dao/ISkillDAO.java
@@ -3,7 +3,7 @@ package entrevista_tecnica.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import entrevista_tecnica.dto.Skill;
-
+//hola
 public interface ISkillDAO extends JpaRepository<Skill, Integer>{
 
 }
